c88a5f76f1812468236033644a7d1768615d7a7f
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-09-19 13:11:55 -0400

    publicado en la nube

---
+++ ParcialLuciaSosa/unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35280990-654f-4401-9ec8-9d19c4ffcce5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParcialLuciaSosa/unittest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodget()
+        {
+            //arange
+            CountriesController countriescontroller = new CountriesController();
+            //act
+
+            var resultado = countriescontroller.GetCountries();
+
+            //assert
+            Assert.IsNotNull(resultado);
+        }
+    }
+}
